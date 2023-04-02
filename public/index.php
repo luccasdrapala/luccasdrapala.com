7e46c26fb86e98be44224be6ece4c6fb1790ad75
unknown <luccasdrapala@gmail.com>
2023-04-01 23:21:47 -0300

    construindo header com bootstrap

---
+++ public/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Luccas Drapala DEV</title>
+
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+
+</head>
+<body class="">
+    <header id="header">
+        <nav class="navbar navbar-expand-md fixed-top bg-dark" data-bs-theme="dark">
+            <div class="container-fluid">
+                <!-- LOGO -->
+                <a class="navbar-brand ms-5" href="index.php">
+                    <img src="assets/images/logo-bg.png" width="250">
+                </a> 
+
+                <!-- TOOGLE -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <!-- UL -->
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav m-auto ">
+                        <li class="nav-item px-5">
+                            <a class="nav-link" href="#">Inicio</a>
+                        </li>
+                        <li class="nav-item px-4">
+                            <a class="nav-link" href="#">Sobre</a>
+                        </li>
+                        <li class="nav-item px-4">
+                            <a class="nav-link" href="#">Trabalho</a>
+                        </li>
+                        <li class="nav-item px-4">
+                            <a class="nav-link" href="#">Tecnologias</a>
+                        </li>
+                        <li class="nav-item px-4">
+                            <a class="nav-link" href="#">Projetos</a>
+                        </li>
+                        <li class="nav-item px-4">
+                            <a class="nav-link" href="#">Contato</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
